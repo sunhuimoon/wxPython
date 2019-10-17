b82e9f2976941f0dae739c60deeacb0661536969
sunhui <1161186762@qq,com>
2019-10-17 13:17:13 +0800

    1

--- wxmain/wx001.py
+++ wxmain/wx001.py
@@ -1,10 +1,22 @@
 import wx
 app = wx.App()
 win = wx.Frame(None,title = "编辑器", size=(410,335))
-win.Show()
+bkg = wx.Panel(win)
 
-loadButton = wx.Button(win, label = '打开',pos = (225,5),size = (80,25))
-saveButton = wx.Button(win, label = '保存',pos = (315,5),size = (80,25))
-filename = wx.TextCtrl(win, pos = (5,5),size = (210,25))
-contents = wx.TextCtrl(win, pos = (5,35),size = (390,260), style = wx.TE_MULTILINE | wx.HSCROLL)
+loadButton = wx.Button(bkg, label = '打开')
+saveButton = wx.Button(bkg, label = '保存')
+filename = wx.TextCtrl(bkg)
+contents = wx.TextCtrl(bkg, style = wx.TE_MULTILINE | wx.HSCROLL)
+
+hbox = wx.BoxSizer()
+hbox.Add(filename, proportion =1, flag = wx.EXPAND)
+hbox.Add(loadButton, proportion =0,flag = wx.LEFT, border = 5)
+hbox.Add(saveButton, proportion =0,flag = wx.LEFT, border = 5)
+
+vbox = wx.BoxSizer(wx.VERTICAL)
+vbox.Add(hbox,proportion = 0,flag = wx.EXPAND | wx.ALL, border = 5)
+vbox.Add(contents, proportion = 1,flag=wx.EXPAND | wx.LEFT | wx.BOTTOM | wx.RIGHT, border = 5)
+
+bkg.SetSizer(vbox)
+win.Show()
 app.MainLoop()
