a5818d8b81db442be399a24f2e2e5316076fa689
sunhui <1161186762@qq,com>
2019-10-17 13:14:34 +0800

    1

--- wxmain/wx001.py
+++ wxmain/wx001.py
@@ -1,5 +1,10 @@
-import wx  # 需要导入wx模块
+import wx
 app = wx.App()
-win = wx.Frame(None)
+win = wx.Frame(None,title = "编辑器", size=(410,335))
 win.Show()
+
+loadButton = wx.Button(win, label = '打开',pos = (225,5),size = (80,25))
+saveButton = wx.Button(win, label = '保存',pos = (315,5),size = (80,25))
+filename = wx.TextCtrl(win, pos = (5,5),size = (210,25))
+contents = wx.TextCtrl(win, pos = (5,35),size = (390,260), style = wx.TE_MULTILINE | wx.HSCROLL)
 app.MainLoop()
